e8b7e63eff7a278907693061d41597ceede44892
theaman249 <theaman249@gmail.com>
2024-09-26 17:32:05 +0200

    Business Dashboard fixed

--- src/frontend/src/lib/components/modules/admin/dashboard/Dashboard.svelte
+++ src/frontend/src/lib/components/modules/admin/dashboard/Dashboard.svelte
@@ -3,14 +3,18 @@
   import ViewProducts from "$lib/components/modules/admin/ViewProducts/ViewProducts.svelte";
   import AddProduct from "$lib/components/modules/admin/AddProduct/AddProduct.svelte";
   import { fullName } from "$lib/data/stores/stores";
+  import * as Tabs from "$lib/components/ui/tabs";
   import { onMount } from "svelte";
+  import * as Card from "$lib/components/ui/card/index.js";
 
   onMount(()=>{
 
     console.log('Working!!');
   });
 
-  let chosen = "Add Products";
+  let chosen = "Dashboard"; 
+
+  //let chosen = "Add Products";
 </script>
 
 <div
@@ -19,64 +23,60 @@
   <div class="col-span-12 h-fit grid grid-cols-12 w-full overflow-scroll mt-5">
     <div class="col-span-12">
       <h1 class="text-2xl font-bold">Welcome {$fullName}!</h1>
-      <p class="text-lg border-b-2 border-zinc-300 pb-5 mt-2 opacity-75">
-        Manage your products and preferences here.
-      </p>
-    </div>
-    <div class="col-span-12 grid grid-cols-12">
-      <div class="col-span-12 lg:hidden flex px-2 justify-around mt-5">
-        <div>
-          <Button
-            class="w-full my-1 justify-start hover:underline {chosen ===
-            'Add Products'
-              ? 'bg-mute'
-              : ''}"
-            variant="ghost"
-            on:click={() => (chosen = "Add Products")}>Add Producs</Button
-          >
+
+      <Tabs.Root value="Dashboard" class=" space-y-1 mt-3">
+        <Tabs.List>
+          <Tabs.Trigger value="Dashbaord">Dashboard</Tabs.Trigger>
+          <Tabs.Trigger value="Add">Add Products</Tabs.Trigger>
+          <Tabs.Trigger value="View">View Products</Tabs.Trigger>
+
+        </Tabs.List>
+        <Tabs.Content value="Dashboard">
+            <div class="flex">
+              </div>
+              <div class="space-y-1 w-1/4 h-full ">
+                <div class="space-y-2 space-x-2 mt-2"> 
+                 DASHBOARD
+                          <!-- come code for dashboard here -->
+
+                </div>
+            </div>
+            
+        </Tabs.Content>
+        <!-- come code for Add Products here -->
+        <Tabs.Content value="Add">
+          <div class="flex space-x-2 space-y-1"> 
+            <Card.Root class="col-span-3 w-full">
+              <Card.Header>
+                <Card.Title>Add Products</Card.Title>
+                <Card.Description>View the history of how you spent your Knowledge Tokens.</Card.Description>
+              </Card.Header>
+              <Card.Content>
+                <AddProduct />
+              </Card.Content>
+              </Card.Root>
+          </div>
+        </Tabs.Content>
+
+      <!-- come code for View here -->
+      <Tabs.Content value="View">
+        <div class="flex space-x-2 space-y-1"> 
+          <Card.Root class="col-span-3 w-full">
+            <Card.Header>
+              <Card.Title>View Products</Card.Title>
+              <Card.Description>View the history of how you spent your Knowledge Tokens.</Card.Description>
+            </Card.Header>
+            <Card.Content>
+              <ViewProducts />
+            </Card.Content>
+          </Card.Root>
+          
         </div>
-        <div>
-          <Button
-            class="w-full my-1 justify-start hover:underline {chosen ===
-            'View Products'
-              ? 'bg-mute'
-              : ''}"
-            variant="ghost"
-            on:click={() => (chosen = "View Products")}>View Products</Button
-          >
-        </div>
-      </div>
-      <div class="hidden lg:col-span-2 lg:block mt-10">
-        <div>
-          <Button
-            class="w-full my-1 justify-start hover:underline {chosen ===
-            'Add Products'
-              ? 'bg-secondary text-black dark:text-black'
-              : ''}"
-            variant="ghost"
-            on:click={() => (chosen = "Add Products")}>Add Producs</Button
-          >
-        </div>
-        <div>
-          <Button
-            class="w-full my-1 justify-start hover:underline {chosen ===
-            'View Products'
-              ? 'bg-secondary text-black dark:text-black'
-              : ''}"
-            variant="ghost"
-            on:click={() => (chosen = "View Products")}>View Products</Button
-          >
-        </div>
-      </div>
-      <div
-        class="col-span-12 lg:col-span-10 mt-10 px-2 h-fit overflow-scroll lg:pl-10"
-      >
-        {#if chosen === "Add Products"}
-          <AddProduct />
-        {:else if chosen === "View Products"}
-          <ViewProducts />
-        {/if}
-      </div>
+      </Tabs.Content> 
+      
+      </Tabs.Root>
+
+      
     </div>
   </div>
 </div>
